d3e22a70d622fbec93fe7cb195984a66cf1c977b
Joz-Bruer Quince <quincejozbruer@gmail.com>
2023-04-16 22:14:00 -0700

    install cors

--- app.js
+++ app.js
@@ -1,26 +1,14 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
-
+const cors = require('cors');
 const connectDB = require('./config/db');
 require('dotenv').config();
 
 const app = express();
 
 connectDB();
-app.use((request, response, next) => {
-	response.setHeader('Access-Control-Allow-Origin', '*');
-	response.setHeader(
-		'Access-Control-Allow-Headers',
-		'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
-	);
-	response.setHeader(
-		'Access-Control-Allow-Methods',
-		'GET, POST, PUT, DELETE, PATCH, OPTIONS'
-	);
-	next();
-});
-
+app.use(cors());
 app.use(
 	'/graphql',
 	graphqlHTTP({
